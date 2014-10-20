62d064c69c99f99f8daec0a521ff2c6afbac3853
kouh <kouh28@gmail.com>
2014-10-20 23:04:55 +0900

    Create Funsen.

---
+++ script.js
@@ -0,0 +1,40 @@
+$(function(){
+  var savedData = localStorage.getItem("fusen");
+  var fusenList = JSON.parse(savedData);
+  
+  if(fusenList && (fusenList.constructor != 'Array')){
+    jQuery.each(fusenList,function(){
+      addFusen(this);
+    });
+  }
+  $('#add').on('click',function(){
+    addFusen();
+  });
+
+  $( "#workspace" ).sortable({
+    revert: true
+  });
+
+  //
+});
+
+function addFusen(data){
+  data = data || '';
+  $('#workspace').append(
+    $('<li>')
+      .attr('contenteditable','true')
+      .text(data)
+      .on('click',function(){
+        $(this).focus();
+      })
+      .on('blur',function(){
+        var fusenList = [];
+        $('#workspace li').each(function(){
+          fusenList.push($(this).text());
+        });
+        localStorage.setItem('fusen',JSON.stringify(fusenList));
+        console.log('saved!');
+      })
+  );
+}
+  
